826ee5dc40178acaa7548f543a837a038a959331
Adam Ginsburg <keflavich@gmail.com>
2013-08-11 15:54:07 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+
+if 'develop' in sys.argv:
+    # use setuptools for develop, but nothing else
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='imf',
+      version='0.1',
+      description='IMF toolkit',
+      long_description=long_description,
+      author='Adam Ginsburg',
+      author_email='adam.ginsburg@colorado.edu',
+      url='http://github.com/keflavich/imf/',
+      packages=['imf'],
+     )
