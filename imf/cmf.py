0039fb00672496c804f638e63494d66a1cce9c73
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-06 11:01:03 +0200

    updates, fixes to some plots, but still can't achieve agreement

--- imf/cmf.py
+++ imf/cmf.py
@@ -33,6 +33,7 @@ def pn11_mf(tnow=1, mmin=0.01*u.M_sun, mmax=120*u.M_sun, T0=10*u.K,
                               mmax=mmax.to(u.M_sun).value,
                               silent=True)*u.M_sun
 
+    # sigma_squared of log(rho/rho0)
     sigma_squared = np.log(1+(MS0/2.)**2 * (1+1./beta)**-1)
 
     # two positional parameters: x, s such that pdf=lognorm(x,s)
@@ -56,72 +57,127 @@ def pn11_mf(tnow=1, mmin=0.01*u.M_sun, mmax=120*u.M_sun, T0=10*u.K,
     # core birthdays: "Finally, we associate a random age to each core,
     # assuming for simplicity that the SFR is uniform over time and independent
     # of core mass."
-    age = np.random.random(len(maccr)) * tcross
-    born = age < tnow*tcross
+    birthday = np.random.random(len(maccr)) * tcross
+    born = birthday < tnow*tcross
 
     b=4.-(3./alpham1)
     a=(b-1)/2.
-    taccr = tcross * sigma**((4.-4.*a)/(3.-2.*a)) * (maccr/m0)**((1-a)/(3-2*a))
+    taccr = (tcross * sigma**((4.-4.*a)/(3.-2.*a)) *
+             (maccr/m0)**((1-a)/(3-2*a))).to(u.Myr)
     print("taccr=[{0} - {1}]".format(taccr.to(u.Myr).min(), taccr.to(u.Myr).max()))
 
     c_s = ((constants.k_B * T_mean / (2.34*constants.m_p))**0.5).to(u.km/u.s)
+    print("c_s = {0}".format(c_s))
     mbe = (1.182 * c_s**3 / (constants.G**1.5 * rho**0.5)).to(u.M_sun)
     tbe = (taccr * (maccr/mbe)**(-1/3.)).to(u.s)
     tff = ((3*np.pi/(32*constants.G*rho))**0.5).to(u.s)
     mmax = (maccr * ((tbe+tff)/taccr)**3).to(u.M_sun)
 
     # tnow = number of crossing times
-    mnow = (((tnow*tcross-age)/taccr)**3 * maccr).to(u.M_sun)
+    age = (tnow*tcross-birthday)
+    mnow = ((age/taccr)**3 * maccr).to(u.M_sun)
+    prestellar = age < tbe+tff
+    ltbe = maccr < mbe
+    stellar = (~prestellar) & (~ltbe)
+    forming = age < taccr
     m_f = np.vstack([mmax.value, maccr.value]).min(axis=0)*u.M_sun
-    print("{0} of {1} have mass greater than final at t={2}."
-          " {3} are unborn.".format((mnow>m_f).sum(), len(mnow), tnow*tcross,
-                                    np.sum(~born)))
+
     mnow[mnow > m_f] = m_f[mnow>m_f]
     will_collapse = maccr > mbe/2.
 
-    return mnow[born], m_f[born], will_collapse[born], maccr[born], mbe[born], mmax[born]
+    # We assume that cores that do not reach their BE mass are seen only during
+    # their formation time, taccr,
+    notseen = ltbe & ~forming
 
-def test_pn11(nreal=10, nbins=50):
-    mnow, mf, wc, maccr, mbe, mmax = pn11_mf()
+    core_mass = mnow[born & (~notseen) & (~stellar)].sum()
+    stellar_mass = mnow[stellar].sum()
+    print("{0} of {1} have mass greater than final at t={2}."
+          " {3} are unborn.  {4} are stellar.  {7} are not seen. "
+          "The CFE={5}"
+          " and SFE={6}".format((mnow>m_f).sum(), len(mnow), tnow*tcross,
+                                np.sum(~born), np.sum(stellar),
+                                (core_mass/m0).decompose().value,
+                                (stellar_mass/m0).decompose().value,
+                                notseen.sum()
+         ))
+
+    return mnow[born], m_f[born], will_collapse[born], maccr[born], mbe[born], mmax[born], forming[born]
+
+def test_pn11(nreal=1, nbins=50, **kwargs):
+    mnow, mf, wc, maccr, mbe, mmax, forming = pn11_mf(**kwargs)
     import pylab as pl
     pl.figure(1).clf()
-    toplot = mnow > 0.05*u.M_sun
-    gtmax = maccr > mmax
     ltbe = maccr < mbe
+    # We assume that cores that do not reach their BE mass are seen only during
+    # their formation time, taccr,
+    notseen = ltbe & ~forming
+    toplot = (mnow > 0.05*u.M_sun) & (~notseen)
+    gtmax = maccr > mmax
     btw = (~gtmax) & (~ltbe)
-    pl.loglog(mnow[toplot&gtmax], (maccr/mnow)[toplot&gtmax], 'kd', markerfacecolor='none')
-    pl.loglog(mnow[toplot&ltbe],  (maccr/mnow)[toplot&ltbe], 'r.', markerfacecolor='none')
-    pl.loglog(mnow[toplot&btw],   (maccr/mnow)[toplot&btw], 'b+', markerfacecolor='none')
+    pl.loglog(mnow[toplot & gtmax], (maccr/mnow)[toplot & gtmax], 'kd',
+              markerfacecolor='none')
+    print("{0} have maccr<mbe and m>0.05".format((toplot & ltbe).sum()))
+    pl.loglog(mnow[toplot & ltbe],  (maccr/mnow)[toplot & ltbe], 'r.',
+              markersize=2, alpha=0.5,
+              markerfacecolor='none')
+    pl.loglog(mnow[toplot & btw],   (maccr/mnow)[toplot & btw], 'b+',
+              markerfacecolor='none')
+    pl.gca().set_ylim(0.5, 500)
+    pl.gca().set_xlim(0.05, 50)
 
     pl.figure(2).clf()
-    pl.hist(maccr/mnow, bins=np.logspace(0,2,12), histtype='step', color='k')
+    pl.hist((maccr/mnow)[mnow>0.1*u.M_sun], bins=np.logspace(0,2,12),
+            histtype='step', color='k', log=True)
     pl.hist((maccr/mnow)[mnow>(mbe/2.)], bins=np.logspace(0,2,12),
-            histtype='step', linestyle='dashed', color='k')
+            histtype='step', linestyle='dashed', color='k', log=True)
+    pl.gca().set_xscale('log')
+
+    pl.figure(3).clf()
+    pl.loglog(mnow[toplot & gtmax], (mnow/mbe)[toplot & gtmax], 'kd',
+              markerfacecolor='none')
+    pl.loglog(mnow[toplot & ltbe], (mnow/mbe)[toplot & ltbe], 'r.',
+              markersize=2,
+              markerfacecolor='none', alpha=0.5)
+    pl.loglog(mnow[toplot & btw], (mnow/mbe)[toplot & btw], 'b+',
+              markerfacecolor='none')
+    ct,bn = np.histogram(mnow[(mnow>mbe/2.) & toplot & (maccr<mbe)],
+                         bins=np.logspace(np.log10(0.05), np.log10(20)))
+    ctall,bn = np.histogram(mnow[(mnow>mbe/2.) & toplot],
+                         bins=np.logspace(np.log10(0.05), np.log10(20)))
+    bbn = (bn[1:]+bn[:-1])/2.
+    pl.loglog(bbn, ct/ctall.astype('float'), 'k-')
 
     pl.figure(4)
     pl.clf()
     pl.hist(mnow, bins=np.logspace(-2,2,nbins), histtype='step', log=True,
-            edgecolor='k')
+            edgecolor='k', label='$m$')
     pl.hist(mnow[wc], bins=np.logspace(-2.02,1.98,nbins), histtype='step', log=True,
-            edgecolor='b', facecolor=(0,0,1,0.25))
+            edgecolor='b', facecolor=(0,0,1,0.25), label='$m>m_{BE}/2$')
     pl.hist(mbe, bins=np.logspace(-1.98,2.02, nbins), histtype='step',
-            log=True, linestyle='dashed', color='g')
+            log=True, linestyle='dashed', color='g', label='$m_{BE}$')
     pl.hist(mmax, bins=np.logspace(-1.96,2.04, nbins), histtype='step',
-            log=True, linestyle='dashed', color='m')
+            log=True, linestyle='dashed', color='m', label='$m_{max}$')
     pl.gca().set_xscale('log')
+    pl.legend(loc='best')
 
-    many_realizations = [pn11_mf() for ii in range(nreal)]
-    mnow = np.hstack([x.value for x,y,z,w,v,t in many_realizations]).ravel()
-    mfwc = np.hstack([x[z].value for x,y,z,w,v,t in many_realizations]).ravel()
+    many_realizations = [pn11_mf(**kwargs) for ii in range(nreal)]
+    mnow_many = np.hstack([x.value for x,y,z,w,v,s,t in many_realizations]).ravel()
+    #mfwc = np.hstack([x[z].value for x,y,z,w,v,t in many_realizations]).ravel()
 
-    counts,bins = np.histogram(mf.ravel(), bins=np.logspace(-2,2,nbins*nreal))
+    counts,bins = np.histogram(mnow_many.ravel(), bins=np.logspace(-2,2,nbins*nreal))
     bbins = (bins[:-1]+bins[1:])/2.
     ok = np.log(counts) > 0
     ppars = np.polyfit(np.log(bbins)[ok], np.log(counts)[ok], 1)
     pl.plot(bbins, np.exp(ppars[1]) * bbins**ppars[0], 'r')
-    return counts,bbins
+
+    pl.figure(5).clf()
+    pl.plot(maccr[toplot], mbe[toplot], 'k,')
+
+    #import ipdb; ipdb.set_trace()
+    #return counts,bbins
     #pl.hist(mf.ravel(), bins=np.logspace(-2,2,nbins*nreal), histtype='step', log=True,
     #        edgecolor='r')
     #pl.hist(mfwc.ravel(), bins=np.logspace(-2,2,nbins*nreal), histtype='step',
     #        edgecolor='g',
     #        log=True)
+    return mnow, mf, wc, maccr, mbe, mmax
