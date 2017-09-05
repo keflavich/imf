0c6aeb13fbf1737886f519231a5b825fa3124971
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-09-05 16:57:55 -0600

    several updates to the time variable pmf

--- imf/pmf.py
+++ imf/pmf.py
@@ -11,7 +11,7 @@ from .imf import MassFunction, Chabrier2005, Kroupa
 chabrier2005 = Chabrier2005()
 
 class McKeeOffner_PMF(MassFunction):
-    def __init__(self, j=1, n=1, jf=3/4., mmin=0.033, mmax=3.0, massfunc=chabrier2005, **kwargs):
+    def __init__(self, j=1, n=1, jf=3/4., mmin=0.033, mmax=3.0, imf=chabrier2005, **kwargs):
         """
         """
         self.j = j
@@ -19,10 +19,10 @@ class McKeeOffner_PMF(MassFunction):
         self.n = n
         self.mmin = mmin
         self.mmax = mmax
-        self.massfunc = massfunc
+        self.imf = imf
 
         def den_func(x):
-            return self.massfunc(x)*x**(-self.jf)
+            return self.imf(x)*x**(-self.jf)
         self.denominator = scipy.integrate.quad(den_func, self.mmin, self.mmax, **kwargs)[0]
 
         self.normfactor = 1
@@ -33,7 +33,7 @@ class McKeeOffner_PMF(MassFunction):
 
             def num_func(x, mass_):
                 tf = (1-(mass_/x)**(1-self.j))**0.5
-                return self.massfunc(x)*x**(self.j-self.jf-1) * tf
+                return self.imf(x)*x**(self.j-self.jf-1) * tf
 
             def integrate(lolim, mass_):
                 integral = scipy.integrate.quad(num_func, lolim, self.mmax, args=(mass_,), **kwargs)[0]
@@ -43,7 +43,7 @@ class McKeeOffner_PMF(MassFunction):
 
         else:
             def num_func(x):
-                return self.massfunc(x)*x**(self.j-self.jf-1)
+                return self.imf(x)*x**(self.j-self.jf-1)
 
             def integrate(lolim):
                 integral = scipy.integrate.quad(num_func, lolim, self.mmax, **kwargs)[0]
@@ -63,7 +63,7 @@ class McKeeOffner_PMF(MassFunction):
 class McKeeOffner_2CTC(MassFunction):
     """ 2-component Turbulent Core variant """
     def __init__(self, Rmdot=3.6, j=0.5, jf=3/4., mmin=0.033, mmax=3.0,
-                 massfunc=chabrier2005, **kwargs):
+                 imf=chabrier2005, **kwargs):
         """
         """
         self.j = j
@@ -71,10 +71,10 @@ class McKeeOffner_2CTC(MassFunction):
         self.mmin = mmin
         self.mmax = mmax
         self.Rmdot = Rmdot
-        self.massfunc = massfunc
+        self.imf = imf
 
         def den_func(x):
-            return self.massfunc(x) * (2/((1+Rmdot**2*x**1.5)**0.5+1))
+            return self.imf(x) * (2/((1+Rmdot**2*x**1.5)**0.5+1))
         self.denominator = scipy.integrate.quad(den_func, self.mmin, self.mmax, **kwargs)[0]
 
         self.normfactor = 1
@@ -85,7 +85,7 @@ class McKeeOffner_2CTC(MassFunction):
 
             def num_func(x, mass_):
                 tf = (1-(mass_/x)**(1-self.j))**0.5
-                return self.massfunc(x)*(1./x)**(1-self.j) * (2/((1+self.Rmdot**2*x**1.5)**0.5+1)) * tf
+                return self.imf(x)*(1./x)**(1-self.j) * (2/((1+self.Rmdot**2*x**1.5)**0.5+1)) * tf
 
             def integrate(lolim, mass_):
                 integral = scipy.integrate.quad(num_func, lolim, self.mmax, args=(mass_,), **kwargs)[0]
@@ -95,7 +95,7 @@ class McKeeOffner_2CTC(MassFunction):
 
         else:
             def num_func(x):
-                return self.massfunc(x)*(1./x)**(1-self.j) * (2/((1+self.Rmdot**2*x**1.5)**0.5+1))
+                return self.imf(x)*(1./x)**(1-self.j) * (2/((1+self.Rmdot**2*x**1.5)**0.5+1))
 
             def integrate(lolim):
                 integral = scipy.integrate.quad(num_func, lolim, self.mmax, **kwargs)[0]
@@ -136,7 +136,86 @@ SalpeterPMF_TC = McKeeOffner_SalpeterPMF(j=0.5, jf=0.75, )
 SalpeterPMF_CA = McKeeOffner_SalpeterPMF(j=2/3., jf=1.0, )
 
 kroupa = Kroupa()
-KroupaPMF_IS = McKeeOffner_PMF(j=0, jf=0, massfunc=kroupa)
-KroupaPMF_TC = McKeeOffner_PMF(j=0.5, jf=0.75, massfunc=kroupa)
-KroupaPMF_CA = McKeeOffner_PMF(j=2/3., jf=1.0, massfunc=kroupa)
-KroupaPMF_2CTC = McKeeOffner_2CTC(massfunc=kroupa)
+KroupaPMF_IS = McKeeOffner_PMF(j=0, jf=0, imf=kroupa)
+KroupaPMF_TC = McKeeOffner_PMF(j=0.5, jf=0.75, imf=kroupa)
+KroupaPMF_CA = McKeeOffner_PMF(j=2/3., jf=1.0, imf=kroupa)
+KroupaPMF_2CTC = McKeeOffner_2CTC(imf=kroupa)
+
+class McKeeOffner_AcceleratingSF_PMF(MassFunction):
+    def __init__(self, j=1, n=1, jf=3/4., mmin=0.033, mmax=3.0,
+                 tau=1, # current time, Myr
+                 tm=0.54, # SF timescale, Myr
+                 tf1=0.50, # accretion  timescale for a 1-msun star
+                 imf=chabrier2005, **kwargs):
+        """
+        McKee & Offner 2010 Protostellar Mass Function with an accelerating star formation rate
+        """
+        self.j = j
+        self.jf = jf
+        self.n = n
+        self.tau = tau
+        self.tm = tm
+        self.mmin = mmin
+        self.mmax = mmax
+        self.imf = imf
+        self.tf1 = tf1
+
+        def den_func(x):
+            return self.imf(x)*(1-np.exp(-self.tf1*x**(1-self.jf)/self.tau))/x
+        self.denominator = self.tau * scipy.integrate.quad(den_func, self.mmin, self.mmax, **kwargs)[0]
+
+        self.normfactor = 1
+
+    def __call__(self, mass, tau=None, taper=False, integral_form=False, **kwargs):
+        """
+
+        Parameters
+        ----------
+        tm : float
+            Star formation timescale in Myrs
+        """
+        if tau is None:
+            tau = self.tau
+
+        if taper:
+
+            raise NotImplementedError()
+        #    def num_func(x, mass_):
+        #        tf = (1-(mass_/x)**(1-self.j))**0.5
+        #        return self.massfunc(x)*x**(self.j-self.jf-1) * tf
+
+        #    def integrate(lolim, mass_):
+        #        integral = scipy.integrate.quad(num_func, lolim, self.mmax, args=(mass_,), **kwargs)[0]
+        #        return integral
+
+        #    numerator = np.vectorize(integrate)(np.where(self.mmin < mass, mass, self.mmin), mass)
+
+        else:
+            def num_func(x, mass_):
+                tm = self.tf1 * mass_**(1-self.j) / (x**(self.jf-self.j))
+                return self.imf(x)*x**(self.j-self.jf-1) * np.exp(-tm / tau)
+
+            def integrate(lolim, mass_):
+                integral = scipy.integrate.quad(num_func, lolim, self.mmax, args=(mass_), **kwargs)[0]
+                return integral
+
+            numerator = np.vectorize(integrate)(np.where(self.mmin < mass, mass, self.mmin), mass)
+
+        result = self.tf1 * (1-self.j) * mass**(1-self.j) * numerator / self.denominator
+
+        # it is possible with the time-evolution case to get negative values for high masses;
+        # these should (probably?) just be zero'd
+        result = result * (result>0)
+
+        if integral_form:
+            warnings.warn("The 'integral form' of the Chabrier PMF is not correctly normalized; "
+                          "it is just PMF(m) * m")
+            return result * self.normfactor * mass
+            raise ValueError("Integral version not yet computed")
+        else:
+            return result * self.normfactor
+
+ChabrierPMF_AcceleratingSF_IS = McKeeOffner_AcceleratingSF_PMF(j=0, jf=0, )
+ChabrierPMF_AcceleratingSF_TC = McKeeOffner_AcceleratingSF_PMF(j=0.5, jf=0.75, )
+ChabrierPMF_AcceleratingSF_CA = McKeeOffner_AcceleratingSF_PMF(j=2/3., jf=1.0, )
+#ChabrierPMF_AcceleratingSF_2CTC = McKeeOffner_AcceleratingSF_2CTC()
