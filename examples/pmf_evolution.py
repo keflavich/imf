cf45e0eae0ea6352a34c3e2ed7b5c7aacb93616d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-09-05 16:58:14 -0600

    added pmf statistics (M>10 / total ratios) and evolution plots

---
+++ examples/pmf_evolution.py
@@ -0,0 +1,51 @@
+import imf.imf, imf.pmf, imp
+from imf.pmf import ChabrierPMF_AcceleratingSF_IS, ChabrierPMF_AcceleratingSF_TC, ChabrierPMF_AcceleratingSF_CA#, ChabrierPMF_AcceleratingSF_2CTC
+import pylab as pl
+import numpy as np
+imp.reload(imf.imf)
+imp.reload(imf.pmf)
+imp.reload(imf.imf)
+imp.reload(imf.pmf)
+
+mmin = 0.033
+
+for mmax in (3, 120):
+
+    print("Normalizing.")
+    ChabrierPMF_AcceleratingSF_IS.mmax = mmax
+    ChabrierPMF_AcceleratingSF_TC.mmax = mmax
+    ChabrierPMF_AcceleratingSF_CA.mmax = mmax
+    #ChabrierPMF_AcceleratingSF_2CTC.mmax = mmax
+
+    ChabrierPMF_AcceleratingSF_IS.normalize(log=True, mmin=mmin, mmax=mmax)
+    ChabrierPMF_AcceleratingSF_TC.normalize(log=True, mmin=mmin, mmax=mmax)
+    ChabrierPMF_AcceleratingSF_CA.normalize(log=True, mmin=mmin, mmax=mmax)
+    #ChabrierPMF_AcceleratingSF_2CTC.normalize(log=True, mmin=mmin, mmax=mmax)
+
+
+    chabrier2005 = imf.Chabrier2005()
+    chabrier2005.normalize(log=True, mmin=mmin, mmax=mmax)
+
+    print("Now plotting.")
+    masses = np.logspace(np.log10(mmin), np.log10(mmax), 100)
+
+    for mass_weighted in (True,False):
+        fname = 'mass_weighted' if mass_weighted else '__call__'
+        fig1 = pl.figure(1)
+        fig1.clf()
+        ax = fig1.gca()
+        ax.set_title("Accelerating SF McKee/Offner + Chabrier PMF")
+        ax.loglog(masses, chabrier2005.__getattribute__(fname)(masses), label="IMF", color='k')
+        for tau, lw in zip((0.1, 0.5, 1.0), (1,2,3)):
+            ax.loglog(masses, ChabrierPMF_AcceleratingSF_IS.__getattribute__(fname)(masses, tau=tau), label="IS", color='r', linewidth=lw, linestyle=':')
+            ax.loglog(masses, ChabrierPMF_AcceleratingSF_TC.__getattribute__(fname)(masses, tau=tau), label="TC", color='g', linewidth=lw, linestyle='-.')
+            ax.loglog(masses, ChabrierPMF_AcceleratingSF_CA.__getattribute__(fname)(masses, tau=tau), label="CA", color='y', linewidth=lw, linestyle='-.')
+        #ax.loglog(masses, ChabrierPMF_AcceleratingSF_2CTC.__getattribute__(fname)(masses), label="2CTC", color='b', linestyle='--')
+        ax.set_xlabel("(Proto)Stellar Mass (M$_\odot$)")
+        ax.set_ylabel("m P(m)" if mass_weighted else "Normalized P(M)")
+        ax.axis([mmin, mmax, 1e-4, 1])
+
+        pl.legend(loc='best')
+        pl.savefig('acceleratingSF_pmf_chabrier{0}_mmax{1}.png'
+                   .format("_integral" if mass_weighted else "", int(mmax)),
+                   bbox_inches='tight')
---
+++ examples/pmf_stats.py
@@ -0,0 +1,75 @@
+import imf.imf, imf.pmf, imp
+imp.reload(imf.imf)
+imp.reload(imf.pmf)
+imp.reload(imf.imf)
+imp.reload(imf.pmf)
+from imf.pmf import ChabrierPMF_IS, ChabrierPMF_TC, ChabrierPMF_CA, ChabrierPMF_2CTC
+from imf.pmf import KroupaPMF_IS, KroupaPMF_TC, KroupaPMF_CA, KroupaPMF_2CTC
+from imf.pmf import McKeeOffner_AcceleratingSF_PMF, ChabrierPMF_AcceleratingSF_IS, ChabrierPMF_AcceleratingSF_TC, ChabrierPMF_AcceleratingSF_CA#, ChabrierPMF_AcceleratingSF_2CTC
+import pylab as pl
+import numpy as np
+
+mmin = 0.033
+mmax = 120
+
+print("Normalizing.")
+ChabrierPMF_IS.mmax = mmax
+ChabrierPMF_TC.mmax = mmax
+ChabrierPMF_CA.mmax = mmax
+ChabrierPMF_2CTC.mmax = mmax
+
+ChabrierPMF_IS.normalize(log=False, mmin=mmin, mmax=mmax)
+ChabrierPMF_TC.normalize(log=False, mmin=mmin, mmax=mmax)
+ChabrierPMF_CA.normalize(log=False, mmin=mmin, mmax=mmax)
+ChabrierPMF_2CTC.normalize(log=False, mmin=mmin, mmax=mmax)
+
+ChabrierPMF_AcceleratingSF_IS.mmax = mmax
+ChabrierPMF_AcceleratingSF_TC.mmax = mmax
+ChabrierPMF_AcceleratingSF_CA.mmax = mmax
+#ChabrierPMF_AcceleratingSF_2CTC.mmax = mmax
+
+ChabrierPMF_AcceleratingSF_IS.normalize(log=True, mmin=mmin, mmax=mmax)
+ChabrierPMF_AcceleratingSF_TC.normalize(log=True, mmin=mmin, mmax=mmax)
+ChabrierPMF_AcceleratingSF_CA.normalize(log=True, mmin=mmin, mmax=mmax)
+#ChabrierPMF_AcceleratingSF_2CTC.normalize(log=True, mmin=mmin, mmax=mmax)
+
+
+
+KroupaPMF_IS.mmax = mmax
+KroupaPMF_TC.mmax = mmax
+KroupaPMF_CA.mmax = mmax
+KroupaPMF_2CTC.mmax = mmax
+
+KroupaPMF_IS.normalize(log=False, mmin=mmin, mmax=mmax)
+KroupaPMF_TC.normalize(log=False, mmin=mmin, mmax=mmax)
+KroupaPMF_CA.normalize(log=False, mmin=mmin, mmax=mmax)
+KroupaPMF_2CTC.normalize(log=False, mmin=mmin, mmax=mmax)
+
+chabrier2005 = imf.Chabrier2005()
+chabrier2005.normalize(log=False, mmin=mmin, mmax=mmax)
+kroupa = imf.Kroupa()
+kroupa.normalize(log=False, mmin=mmin, mmax=mmax)
+
+print("Done normalizing")
+
+mfs = {'ChabrierPMF_IS': ChabrierPMF_IS,
+       'ChabrierPMF_TC': ChabrierPMF_TC,
+       'ChabrierPMF_CA': ChabrierPMF_CA,
+       'ChabrierPMF_2CTC': ChabrierPMF_2CTC,
+       'ChabrierIMF': chabrier2005,
+       'KroupaPMF_IS': KroupaPMF_IS,
+       'KroupaPMF_TC': KroupaPMF_TC,
+       'KroupaPMF_CA': KroupaPMF_CA,
+       'KroupaPMF_2CTC': KroupaPMF_2CTC,
+       'KroupaIMF': kroupa,
+      }
+
+for tau in (0.1, 0.5, 1.0):
+    mfs['ChabrierPMF_AcceleratingSF_IS_tau{0}'.format(tau)] = McKeeOffner_AcceleratingSF_PMF(j=0, jf=0, tau=tau)
+    mfs['ChabrierPMF_AcceleratingSF_TC_tau{0}'.format(tau)] = McKeeOffner_AcceleratingSF_PMF(j=0.5, jf=0.75, tau=tau)
+    mfs['ChabrierPMF_AcceleratingSF_CA_tau{0}'.format(tau)] = McKeeOffner_AcceleratingSF_PMF(j=2/3., jf=1.0, tau=tau)
+
+for mf in sorted(mfs):
+    total = mfs[mf].m_integrate(mmin, mmax)[0]
+    gt10 = mfs[mf].m_integrate(10, mmax)[0]
+    print("Mass fraction for {1} M>10 = {0:0.3f}".format(gt10/total, mf))
