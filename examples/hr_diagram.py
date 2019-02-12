d20b542b732513813c422628880e6dbc4e15d58a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-02-12 10:06:00 -0700

    comment the code

--- examples/hr_diagram.py
+++ examples/hr_diagram.py
@@ -1,3 +1,10 @@
+"""
+Create a Hertzprung-Russell (temperature-luminosity) diagram and
+mass-luminosity and mass-temperature diagrams populating the main sequence only
+with data from Ekström+ 2012 (Vizier catalog J/A+A/537/A146/iso).
+
+Colors come from vendian.org.
+"""
 import numpy as np
 import imf
 import pylab as pl
@@ -17,24 +24,28 @@ for age in np.unique(tbl['logAge']):
     agelum[age] = tbl[tbl['logAge']==age]['logL'].max()
 
 
-# hr diagram
+# mass-temperature diagram
 pl.figure(2, figsize=(8,8)).clf()
 
+# select a specific population age to plot - this is the youngest, with all
+# stars alive and main-sequence
 ok = tbl['logAge'] == 6.5
 
 subtbl = tbl[ok]
 subtbl.sort('Mass')
 lowmass = subtbl[subtbl['Mass'] < 2]
 
+# downsample the high-mass part of the sample to avoid point overlap
 subtbl = subtbl[(subtbl['Mass'] < 25) & (subtbl['logTe']<4.6)]
 highmass = subtbl[(subtbl['Mass'] > 20) & (subtbl['logTe']<4.6)]
 subtbl=subtbl[::20]
 
-# fill in low-mass
+# fill in (oversample) the low-mass portion
 Lfit = np.polyfit(np.log10(lowmass['Mass']), lowmass['logL'], 1)
-# not exactly right; there's a break at 0.43ish
+# the fit is not exactly right; there's a break at 0.43ish, but it's good enough
 masses = np.logspace(np.log10(0.1), np.log10(0.8))
 lums = np.poly1d(Lfit)(np.log10(masses))
+# this was an old by-hand fit
 #lums[masses<(0.43)] = np.log10(0.23*(masses[masses<(0.43)])**2.3)
 Tfit = np.polyfit(np.log10(lowmass['Mass']), lowmass['logTe'], 1)
 tems = np.poly1d(Tfit)(np.log10(masses))
@@ -46,8 +57,10 @@ hlums = np.poly1d(Lfit)(np.log10(hmasses))
 Tfit = np.polyfit(np.log10(highmass['Mass']), highmass['logTe'], 1)
 htems = np.poly1d(Tfit)(np.log10(hmasses))
 
+# obtain the colors from the vendian-based table
 colors = [imf.color_from_mass(m) for m in subtbl['Mass']]
-#pl.gca().set_xscale('log')
+
+# start plotting
 pl.gca().set_yscale('log')
 pl.scatter(10**subtbl['logTe'],
            subtbl['Mass'],
@@ -66,6 +79,7 @@ pl.scatter(10**htems,
            c=colors,
            s=(10**hlums)**0.25*45)
 
+# overlay star age horizontal lines
 lines = []
 for age in (6.5, 7, 8, 10):
     L, = pl.plot([10**tems.min(), (10**htems.max())],
@@ -79,6 +93,8 @@ pl.ylabel("Mass")
 pl.tight_layout()
 pl.savefig("mass_lum_diagram.svg")#, bbox_inches='tight')
 
+
+# HR diagram (temperature-luminosity)
 pl.figure(3, figsize=(8,8)).clf()
 
 colors = [imf.color_from_mass(m) for m in subtbl['Mass']]
@@ -101,6 +117,7 @@ pl.scatter(10**htems,
            c=colors,
            s=hmasses*5)
 
+# I attempted to add age lines, but this approach is incorrect
 #suns live 5 Gyr, not 10+ Gyr
 # lines = []
 # for age in (6.5, 7, 8, 10):
@@ -116,6 +133,8 @@ pl.ylabel("Luminosity")
 pl.tight_layout()
 pl.savefig("HR_diagram.svg")#, bbox_inches='tight')
 
+
+# mass-luminosity diagram
 pl.figure(4, figsize=(8,8)).clf()
 
 colors = [imf.color_from_mass(m) for m in subtbl['Mass']]
