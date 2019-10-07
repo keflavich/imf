6ee01a327b73cf127cee45520728e569ae73aaae
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-10-07 17:12:23 -0400

    more test cleaning

--- imf/tests/test_imf.py
+++ imf/tests/test_imf.py
@@ -46,16 +46,18 @@ def test_kroupa_mintegral(mlow, mhigh):
         assert anl != 0
 
 
-def test_chabrier_integral():
-    for mlow in (0.033, 0.5, 1, 1.5, 3):
-        for mhigh in (0.05, 0.5, 1, 1.5, 3.0):
-            try:
-                num = chabrier2005.integrate(mlow, mhigh, numerical=True)[0]
-                anl = chabrier2005.integrate(mlow, mhigh, numerical=False)[0]
-            except ValueError:
-                continue
-            print("{0} {1} {2:0.3f} {3:0.3f}".format(mlow, mhigh, num, anl))
-            np.testing.assert_almost_equal(num, anl)
+@pytest.mark.parametrize(('mlow', 'mhigh'),
+                         itertools.product((0.033, 0.01, 0.08, 0.1, 0.5, 1.0, 0.03),
+                                           (0.02, 0.05, 0.08, 0.4, 0.5, 1.0, 120)))
+def test_chabrier_integral(mlow, mhigh):
+    if mlow >= mhigh:
+        pytest.skip("mmin >= mmax")
+
+    num = chabrier2005.integrate(mlow, mhigh, numerical=True)[0]
+    anl = chabrier2005.integrate(mlow, mhigh, numerical=False)[0]
+
+    print("{0} {1} {2:0.3f} {3:0.3f}".format(mlow, mhigh, num, anl))
+    np.testing.assert_almost_equal(num, anl)
 
     #for mlow in (0.01, 0.08, 0.1, 0.5, 1.0):
     #    for mhigh in (0.02, 0.08, 0.4, 0.5, 1.0):
@@ -69,10 +71,10 @@ def test_chabrier_integral():
 
 
 def test_krouva_val():
-    assert np.allclose(kroupa(1) , 0.09143468328964573,rtol=1e-4,atol=1e-4)
-    assert np.allclose(kroupa(0.05), 5.615132028768199, rtol=1e-3,atol=1e-3)
+    assert np.allclose(kroupa(1), 0.09143468328964573, rtol=1e-4, atol=1e-4)
+    assert np.allclose(kroupa(0.05), 5.615132028768199, rtol=1e-3, atol=1e-3)
     assert np.allclose(kroupa(1.5), 0.03598330658344697, rtol=1e-4, atol=1e-4)
-    assert np.allclose(kroupa(3), 0.007306881750306478, rtol=1e-4,atol=1e-4)
+    assert np.allclose(kroupa(3), 0.007306881750306478, rtol=1e-4, atol=1e-4)
 
 def test_make_cluster():
     cluster = imf.make_cluster(1000)
