335d281b6992912bca29f8e5a83f3d66f8cf0a73
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-11-15 16:26:54 -0500

    major updates to light calculations

--- examples/mass_to_light.py
+++ examples/mass_to_light.py
@@ -1,50 +1,118 @@
 import numpy as np
+import os
+import json
 from imf import imf
 import pylab as pl
+import matplotlib
 from astropy.utils.console import ProgressBar
 
-clusters, luminosities, masses, mean_luminosities, mean_masses, max_masses = {},{},{},{},{},{}
+if os.path.exists('synth_data_m_to_l.json'):
+    with open('synth_data_m_to_l.json', 'r') as fh:
+        synth_data = json.load(fh)
+else:
+    synth_data = {}
+
 # uniform random sampling from 100 to 10^5 msun
-for clmass in ProgressBar(np.concatenate([10**(np.random.rand(int(1e3))*1 + 4), 10**(np.random.rand(int(1e4))*2.5+1.5)])):
-    clusters[clmass] = imf.make_cluster(clmass, 'kroupa', mmax=150, silent=True)
-    # cluster luminosities
-    luminosities[clmass] = imf.lum_of_cluster(clusters[clmass])
-    masses[clmass] = clmass
-    mean_luminosities[clmass] = np.mean(luminosities[clmass])
-    mean_masses[clmass] = np.mean(clusters[clmass])
-    max_masses[clmass] = np.max(clusters[clmass])
+for stop_crit in ('nearest', 'before', 'after', 'sorted'):
+    print(stop_crit)
+    if stop_crit not in synth_data:
+        clusters, luminosities, masses, mean_luminosities, mean_masses, max_masses, number = {},{},{},{},{},{},{}
+        for clmass in ProgressBar(np.concatenate([10**(np.random.rand(int(1e3))*1 + 4), 10**(np.random.rand(int(1e4))*2.5+1.5)])):
+            clusters[clmass] = imf.make_cluster(clmass, 'kroupa', mmax=150, silent=True, stop_criterion=stop_crit)
+            # cluster luminosities
+            luminosities[clmass] = imf.lum_of_cluster(clusters[clmass])
+            masses[clmass] = clmass
+            number[clmass] = len(clusters[clmass])
+            #mean_luminosities[clmass] = np.mean(luminosities[clmass])
+            mean_masses[clmass] = np.mean(clusters[clmass])
+            max_masses[clmass] = np.max(clusters[clmass])
+
+        synth_data[stop_crit] = {#'clusters': clusters,
+                                 'number': number,
+                                 'luminosities': luminosities,
+                                 'masses': masses,
+                                 'mean_luminosities': mean_luminosities,
+                                 'mean_masses': mean_masses,
+                                 'max_masses': max_masses}
+    else:
+        max_masses = synth_data[stop_crit]['max_masses']
+        #mean_luminosities = synth_data[stop_crit]['mean_luminosities']
+        mean_masses = synth_data[stop_crit]['mean_masses']
+        number = synth_data[stop_crit]['number']
+        masses = synth_data[stop_crit]['masses']
+        luminosities = synth_data[stop_crit]['luminosities']
+
+    clmasses = sorted(map(float, masses))
+
+    mass_to_light = np.array([k/10**luminosities[str(k)] for k in clmasses])
+
+    # shouldn't this converge to a single value? (yes, don't divide linear by log)
+    pl.figure(2).clf()
+    pl.loglog(clmasses, mass_to_light**-1, '.', alpha=0.1)
+    pl.xlabel("Cluster Mass")
+    pl.ylabel("Light to Mass $L_\odot / M_\odot$")
+    pl.ylim(1,5e4)
+    pl.savefig(f"light_to_mass_vs_mass_{stop_crit}.png", bbox_inches='tight', dpi=200)
+    pl.savefig(f"light_to_mass_vs_mass_{stop_crit}.pdf", bbox_inches='tight')
 
 
-mass_to_light = np.array([mean_masses[k]/mean_luminosities[k] for k in sorted(clusters.keys())])
+    pl.figure(3).clf()
+    pl.loglog(list(map(float, max_masses.keys())), max_masses.values(), '.', alpha=0.1)
+    pl.xlabel("Cluster Mass")
+    pl.ylabel("Maximum stellar mass")
+    pl.savefig(f"maxmass_vs_clustermass_{stop_crit}.png", bbox_inches='tight', dpi=200)
+    pl.savefig(f"maxmass_vs_clustermass_{stop_crit}.pdf", bbox_inches='tight')
 
-pl.figure(2).clf()
-pl.semilogx(sorted(clusters.keys()), mass_to_light**-1, '.', alpha=0.1)
-pl.xlabel("Cluster Mass")
-pl.ylabel("Light to Mass $L_\odot / M_\odot$")
-pl.ylim(6,21)
-pl.savefig("light_to_mass_vs_mass.pdf")
+    m_to_ls = []
+    slopes = np.linspace(1.7, 2.9, 20)
+    for slope in ProgressBar(slopes):
+        Kroupa = imf.Kroupa(p3=slope)
+        cluster = imf.make_cluster(1e5, Kroupa, mmax=120, silent=True,
+                                   stop_criterion=stop_crit)
+        lum = imf.lum_of_cluster(cluster)
+        m_to_l = 1e5/10**lum
+        m_to_ls.append(m_to_l)
+
+    pl.rc('font', size=16)
+    pl.figure(4).clf()
+    pl.plot(slopes, m_to_ls)
+    pl.xlabel("Upper-end power-law slope $\\alpha$")
+    pl.ylabel("Mass-to-light ratio [M$_\odot$/L$_\odot$]")
+    ax = pl.gca()
+    tw = ax.twinx()
+    tw.set_ylim(m_to_ls[0]/m_to_ls[10], m_to_ls[-1]/m_to_ls[10])
+    tw.set_ylabel("M/L / M/L($\\alpha=2.3$)")
+
+    pl.savefig(f"masstolight_vs_slope_{stop_crit}.pdf", bbox_inches='tight')
+    pl.savefig(f"masstolight_vs_slope_{stop_crit}.png", bbox_inches='tight', dpi=200)
 
 
-pl.figure(3).clf()
-pl.loglog(max_masses.keys(), max_masses.values(), '.', alpha=0.1)
-pl.xlabel("Cluster Mass")
-pl.ylabel("Maximum stellar mass")
-pl.savefig("maxmass_vs_clustermass.pdf")
 
-m_to_ls = []
-slopes = np.linspace(1.7, 2.9, 20)
-for slope in slopes:
-    Kroupa = imf.Kroupa(p3=slope)
-    cluster = imf.make_cluster(1e5, Kroupa, mmax=120, silent=True)
-    lum = imf.lum_of_cluster(cluster)
-    m_to_l = 1e5/lum
-    m_to_ls.append(m_to_l)
+    pl.rc('font', size=16)
+    pl.figure(4).clf()
+    pl.semilogy(slopes, m_to_ls)
+    ax = pl.gca()
+    ylim = ax.get_ylim()
+    ax.vlines(2.3, 1e-5, 1, linestyle='--', color='k', alpha=0.2, zorder=-10)
+    ax.set_ylim(ylim)
+    pl.xlabel("Upper-end power-law slope $\\alpha$")
+    pl.ylabel("Mass-to-light ratio [M$_\odot$/L$_\odot$]")
+    tw = ax.twinx()
+    tw.semilogy()
+    tw.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    tw.set_yticks([0.5,1,2,5])
+    tw.set_ylim(m_to_ls[0]/m_to_ls[10], m_to_ls[-1]/m_to_ls[10])
+    tw.set_xlim(ax.get_xlim())
+    tw.hlines(1, 1, 5, linestyle='--', color='k', alpha=0.2, zorder=-20)
+    #tw.grid(which='major', linestyle='--')
+    tw.set_ylabel("M/L / M/L($\\alpha=2.3$)")
 
-pl.figure(4).clf()
-pl.plot(slopes, m_to_ls)
-pl.xlabel("Upper-end power-law slope $\\alpha$")
-pl.ylabel("Mass-to-light ratio [M$_\odot$/L$_\odot$]")
+    pl.savefig(f"masstolight_vs_slope_log_{stop_crit}.pdf", bbox_inches='tight')
+    pl.savefig(f"masstolight_vs_slope_log_{stop_crit}.png", bbox_inches='tight', dpi=200)
 
-#pl.loglog(clusters.keys(), np.array(list(mean_luminosities.values())) / np.array(list(mean_masses.values())), '.', alpha=0.1)
+    #pl.loglog(clusters.keys(), np.array(list(mean_luminosities.values())) / np.array(list(mean_masses.values())), '.', alpha=0.1)
 
-# compute mass-to-light ratio vs age
+    # compute mass-to-light ratio vs age
+
+with open('synth_data_m_to_l.json', 'w') as fh:
+    json.dump(synth_data, fh)
--- imf/imf.py
+++ imf/imf.py
@@ -470,52 +470,95 @@ def make_cluster(mcluster, massfunc='kroupa', verbose=False, silent=False,
 
     return masses
 
-# Vacca Garmany Shull log(lyman continuum) parameters
-# Power-law extrapolated from 18 to 8 and from 50 to 150
-# (using, e.g., ",".join(["%0.2f" % p for p in polyval(polyfit(log10(vgsmass[:5]),vgslogq[:5],1),log10(linspace(50,150,6)))[::-1]])
-# where vgsmass does *not* include the extrapolated values)
-vgsmass = [150.,  130.,  110.,   90.,   70.,  51.3,44.2,41.0,38.1,35.5,33.1,30.8,28.8,26.9,25.1,23.6,22.1,20.8,19.5,18.4,18.,  16.,  14.,  12.,  10.,   8.][::-1]
-vgslogq = [50.51,50.34,50.13,49.88,49.57,49.18,48.99,48.90,48.81,48.72,48.61,48.49,48.34,48.16,47.92,47.63,47.25,46.77,46.23,45.69,45.58,44.65,43.60,42.39,40.96,39.21][::-1]
+mass_luminosity_interpolator_cache = {}
 
-# non-extrapolated
-vgsM    = [51.3,44.2,41.0,38.1,35.5,33.1,30.8,28.8,26.9,25.1,23.6,22.1,20.8,19.5,18.4]
-vgslogL = [6.154,6.046,5.991,5.934,5.876,5.817,5.756,5.695,5.631,5.566,5.499,5.431,5.360,5.287,5.211]
-vgslogQ = [49.18,48.99,48.90,48.81,48.72,48.61,48.49,48.34,48.16,47.92,47.63,47.25,46.77,46.23,45.69]
-# mass extrapolated
-vgsMe = np.concatenate([
-    np.linspace(0.03,0.43,100),
-    np.linspace(0.43,2,100),
-    np.linspace(2,20,100),
-    vgsM[::-1],
-    np.linspace(50,150,100)])
-# log luminosity extrapolated
-vgslogLe = np.concatenate([
-    np.log10(0.23*np.linspace(0.03,0.43,100)**2.3),
-    np.log10(np.linspace(0.43,2,100)**4),
-    np.log10(1.5*np.linspace(2,20,100)**3.5),
-    vgslogL[::-1],
-    np.polyval(np.polyfit(np.log10(vgsM)[:3],vgslogL[:3],1),np.log10(np.linspace(50,150,100)))])
-# log Q (lyman continuum) extrapolated
-vgslogQe = np.concatenate([
-    np.zeros(100), # 0.03-0.43 solar mass stars produce 0 LyC photons
-    np.zeros(100), # 0.43-2.0 solar mass stars produce 0 LyC photons
-    np.polyval(np.polyfit(np.log10(vgsM)[-3:],vgslogQ[-3:],1),np.log10(np.linspace(8,18.4,100))),
-    vgslogQ[::-1],
-    np.polyval(np.polyfit(np.log10(vgsM)[:3],vgslogQ[:3],1),np.log10(np.linspace(50,150,100)))])
+def mass_luminosity_interpolator(name):
+    if name in mass_luminosity_interpolator_cache:
+        return mass_luminosity_interpolator_cache[name]
+    elif name == 'VGS':
+        # Vacca Garmany Shull log(lyman continuum) parameters
+        # Power-law extrapolated from 18 to 8 and from 50 to 150
+        # (using, e.g., ",".join(["%0.2f" % p for p in polyval(polyfit(log10(vgsmass[:5]),vgslogq[:5],1),log10(linspace(50,150,6)))[::-1]])
+        # where vgsmass does *not* include the extrapolated values)
+        vgsmass = [150.,  130.,  110.,   90.,   70.,  51.3,44.2,41.0,38.1,35.5,33.1,30.8,28.8,26.9,25.1,23.6,22.1,20.8,19.5,18.4,18.,  16.,  14.,  12.,  10.,   8.][::-1]
+        vgslogq = [50.51,50.34,50.13,49.88,49.57,49.18,48.99,48.90,48.81,48.72,48.61,48.49,48.34,48.16,47.92,47.63,47.25,46.77,46.23,45.69,45.58,44.65,43.60,42.39,40.96,39.21][::-1]
 
-def lum_of_star(mass):
+        # non-extrapolated
+        vgsM    = [51.3,44.2,41.0,38.1,35.5,33.1,30.8,28.8,26.9,25.1,23.6,22.1,20.8,19.5,18.4]
+        vgslogL = [6.154,6.046,5.991,5.934,5.876,5.817,5.756,5.695,5.631,5.566,5.499,5.431,5.360,5.287,5.211]
+        vgslogQ = [49.18,48.99,48.90,48.81,48.72,48.61,48.49,48.34,48.16,47.92,47.63,47.25,46.77,46.23,45.69]
+        # mass extrapolated
+        vgsMe = np.concatenate([
+            np.linspace(0.03,0.43,100),
+            np.linspace(0.43,2,100),
+            np.linspace(2,20,100),
+            vgsM[::-1],
+            np.linspace(50,150,100)])
+        # log luminosity extrapolated
+        vgslogLe = np.concatenate([
+            np.log10(0.23*np.linspace(0.03,0.43,100)**2.3),
+            np.log10(np.linspace(0.43,2,100)**4),
+            np.log10(1.5*np.linspace(2,20,100)**3.5),
+            vgslogL[::-1],
+            np.polyval(np.polyfit(np.log10(vgsM)[:3],vgslogL[:3],1),np.log10(np.linspace(50,150,100)))])
+        # log Q (lyman continuum) extrapolated
+        vgslogQe = np.concatenate([
+            np.zeros(100), # 0.03-0.43 solar mass stars produce 0 LyC photons
+            np.zeros(100), # 0.43-2.0 solar mass stars produce 0 LyC photons
+            np.polyval(np.polyfit(np.log10(vgsM)[-3:],vgslogQ[-3:],1),np.log10(np.linspace(8,18.4,100))),
+            vgslogQ[::-1],
+            np.polyval(np.polyfit(np.log10(vgsM)[:3],vgslogQ[:3],1),np.log10(np.linspace(50,150,100)))])
+
+        mass_luminosity_interpolator_cache[name] = vgsMe, vgslogLe, vgslogQ
+
+        return mass_luminosity_interpolator_cache[name]
+    elif name == 'Ekstrom':
+        from astroquery.vizier import Vizier
+        Vizier.ROW_LIMIT = 1e7 # effectively infinite
+
+        # this query should cache
+        tbl = Vizier.get_catalogs('J/A+A/537/A146/iso')[0]
+
+        match = tbl['logAge'] == 6.5
+        masses = tbl['Mass'][match]
+        lums = tbl['logL'][match]
+        mass_0 = 0.033
+        lum_0 = np.log10((mass_0/masses[0])**3.5 * 10**lums[0])
+        mass_f = 200 # extrapolate to 200 Msun...
+        lum_f = np.log10(10**lums[-1] * (mass_f/masses[-1])**1.35)
+
+        masses = np.array([mass_0] + masses.tolist() + [mass_f])
+        lums = np.array([lum_0] + lums.tolist() + [lum_f])
+
+        # TODO: come up with a half-decent approximation here?  based on logTe?
+        logQ = lums - 0.5
+
+        mass_luminosity_interpolator_cache[name] = masses, lums, logQ
+
+        return mass_luminosity_interpolator_cache[name]
+    else:
+        raise ValueError("Bad grid name {0}".format(name))
+
+def lum_of_star(mass, grid='Ekstrom'):
     """
     Determine total luminosity of a star given its mass
+
+    Two grids:
+        (1) VGS:
     Uses the Vacca, Garmany, Shull 1996 Table 5 Log Q and Mspec parameters
 
     returns LogL in solar luminosities
     **WARNING** Extrapolates for M not in [18.4,50] msun
 
     http://en.wikipedia.org/wiki/Mass%E2%80%93luminosity_relation
-    """
-    return np.interp(mass, vgsMe, vgslogLe)
 
-def lum_of_cluster(masses):
+    (2) Ekstrom 2012:
+    Covers 0.8 - 64 Msun, extrapolated out of that
+    """
+    masses, lums, _ = mass_luminosity_interpolator(grid)
+    return np.interp(mass, masses, lums)
+
+def lum_of_cluster(masses, grid='Ekstrom'):
     """
     Determine the log of the integrated luminosity of a cluster
     Only M>=8msun count
@@ -523,7 +566,7 @@ def lum_of_cluster(masses):
     masses is a list or array of masses.
     """
     #if max(masses) < 8: return 0
-    logL = lum_of_star(masses) #[masses >= 8])
+    logL = lum_of_star(masses, grid=grid) #[masses >= 8])
     logLtot = np.log10( (10**logL).sum() )
     return logLtot
 
@@ -534,8 +577,9 @@ def lyc_of_star(mass):
 
     returns LogQ
     """
+    masses, _, logQ =  mass_luminosity_interpolator(grid)
 
-    return np.interp(mass, vgsMe, vgslogQe)
+    return np.interp(mass, masses, logQ)
 
 def lyc_of_cluster(masses):
     """
